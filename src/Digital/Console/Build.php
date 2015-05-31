51f5dea92ebec8c97a1e8acb8c59738ba665f25e
Mike Bell <hello@mikebell.io>
2015-05-31 14:59:20 +0100

    Add output to build process

--- src/Digital/Console/Build.php
+++ src/Digital/Console/Build.php
@@ -18,12 +18,13 @@ class Build extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $text = 'Built html documentation';
         $file = json_decode(file_get_contents('output/commands.json'));
         exec("rm drush.docset/Contents/Resources/docSet.dsidx");
+        print 'Removed database' . PHP_EOL;
         $db = new sqlite3("drush.docset/Contents/Resources/docSet.dsidx");
         $db->query("CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT)");
         $db->query("CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path)");
+        print 'Created database' . PHP_EOL;
         foreach ($file as $key => $contents) {
             foreach ($contents as $command) {
                 if (is_object($command)) {
@@ -38,9 +39,11 @@ class Build extends Command
                 }
             }
         }
+        print 'Built html documentation' . PHP_EOL;
         exec("tar --exclude='.DS_Store' -cvzf Drush.tgz drush.docset");
         print 'Built Drush.tgz' . PHP_EOL;
 
+        $text = 'Complete'
         $output->writeln($text);
     }
 }
