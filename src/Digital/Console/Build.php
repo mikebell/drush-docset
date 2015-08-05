df5e11d56f7658889053e60a90e7bc6e01a2b2d6
Mike Bell <hello@mikebell.io>
2015-08-05 12:17:14 +0100

    Added another line to funky

--- src/Digital/Console/Build.php
+++ src/Digital/Console/Build.php
@@ -64,7 +64,12 @@ class Build extends Command
         }
         exec("tar --exclude='.DS_Store' -cvzf Drush.tgz drush.docset");
 
-        $output->writeln('Deleting old index.htm');
+        if ($input->getOption('funky')) {
+            $output->writeln('<info>Deleting old index.htm</info>');
+        } else {
+            $output->writeln('Deleting old index.htm');
+        }
+
         exec("rm drush.docset/Contents/Resources/Documents/index.htm");
 
         Twig_Autoloader::register();
